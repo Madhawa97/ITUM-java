029d0ab3824c57c9d4804dc0d9252273c6715c37
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-02-16 10:04:12 +0530

    Add encapsulation example

--- src/com/itum/Student.java
+++ src/com/itum/Student.java
@@ -1,2 +1,24 @@
-package com.itum;public class Student {
+package com.itum;
+
+public class Student {
+    private String name;
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    // Cmd+ N to automatically get setters and getters
 }
+
+class TestEncapsulation{
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Tharaka");
+        System.out.println(s.getName());
+    }
+}
+
