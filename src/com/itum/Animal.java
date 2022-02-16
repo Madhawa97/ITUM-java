86b2fda5b41bc1b66779727f5b23682e4ac6ff7e
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-02-16 10:01:49 +0530

    Add abstraction example

--- src/com/itum/Animal.java
+++ src/com/itum/Animal.java
@@ -1,2 +1,33 @@
-package com.itum;public class Animal {
+package com.itum;
+
+abstract class Animal {
+
+    // they can have normal methods
+    // they can't have implementation of a abstract method
+
+    public void sleep(){
+        System.out.println("Animal is Sleeping...");
+    }
+
+    public abstract void animalSound(); // can't implement this method here.
+
+}
+
+//class Lion extends Animal { // as soon as you extend, you get an error. YOU MUST IMPLEMENT THE METHOD BODY HERE.
+//}
+
+class Lion extends Animal {
+    public void animalSound(){
+        System.out.println("The Lion is Roaring...");
+    }
+}
+
+
+class TestAnimal {
+    public static void main(String[] args) {
+
+        Lion lion = new Lion();
+        lion.animalSound();
+        lion.sleep();               // calling from super class
+    }
 }
