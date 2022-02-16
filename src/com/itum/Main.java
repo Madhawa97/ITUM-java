b9390456424e22df0eaa1ac341e54e47028d8e4c
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-02-16 09:57:18 +0530

    Added Constructor example

--- src/com/itum/Main.java
+++ src/com/itum/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        ABC obj = new ABC(); // ABC() actually we're calling the constructor
+
+    }
+}
+
+
+class ABC {
+    public ABC() { // this is a constructor of ABC class
+        System.out.println("This is ABC constructor");  // at the object creation we call the constructor
     }
 }
